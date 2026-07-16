bd953f8622cfe8b3a7bde1d78a6ed434ade24195
Kyuu449977 <>
2026-07-16 13:41:53 +0700

    fix

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost/hospital_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:123456@localhost/hospital_db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
